c7025760f9f21847e003e082b7aa533770b10645
avery <avry@pm.me>
2025-06-08 01:03:40 -0700

    flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            sqlite
+            pkg-config
+          ];
+          
+          LIBSQLITE3_SYS_USE_PKG_CONFIG = "1";
+        };
+      }
+    );
+}
